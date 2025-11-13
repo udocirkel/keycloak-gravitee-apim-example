f211392a0283413545f1c045707dc0495ba63bdc
Udo Cirkel <u.cirkel@verband.creditreform.de>
2025-11-14 00:09:07 +0100

    Improve docker compose configs and scripts

--- stop.sh
+++ stop.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-if [ -z "$(docker ps -a -q)" ]; then
-  echo "Keine Container vorhanden."
-else
-  echo "Container werden gestoppt."
-  docker stop $(docker ps -a -q)
+echo ""
+echo "Stopping Keycloak, Coffee Service, Gravitee APIM containers"
+echo "-----------------------------------------------------------"
+docker compose down
 
-  echo "Container werden gelöscht."
-  docker rm $(docker ps -a -q)
+echo ""
+echo "Checking container status"
+echo "-------------------------"
+docker ps -a
 
-  echo "Container werden geprüft."
-  docker ps -a
-fi
+echo ""
