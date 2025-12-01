4adecffc294a228330a9ddfa70fdbdb25eeb48b0
Udo Cirkel <mail@udocirkel.de>
2025-12-01 21:52:31 +0100

    Populate repository

---
+++ coffeehouse/coffee-ingredient-service/src/main/java/de/udocirkel/example/kcgravitee/coffeehouse/ingredient/CoffeeIngredientServiceApplication.java
@@ -0,0 +1,12 @@
+package de.udocirkel.example.kcgravitee.coffeehouse.ingredient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeIngredientServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeIngredientServiceApplication.class, args);
+    }
+
+}
